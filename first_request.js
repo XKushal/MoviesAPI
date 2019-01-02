aae2d0ee6a12d22c256b2cff0f521a0bd90627f6
Xkushal <kushalyhakuri123@gmail.com>
2019-01-02 14:07:34 -0600

    first request made

--- first_request.js
+++ first_request.js
@@ -1,6 +1,11 @@
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
-  };
+request('http://www.google.com', function(error, response, body) {
+    if(error){
+        console.log("SOMETHING WENT WRONG.");
+        console(error);
+    }else{
+        if(response.statusCode == 200){
+            console.log(body);
+        }
+    }
 });
